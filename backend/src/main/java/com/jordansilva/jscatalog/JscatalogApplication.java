24741ecf1622f0512293d5453e5bf60035e9b4f9
Jordan Silva <jordanrafael92@gmail.com>
2021-09-21 18:54:09 -0300

    Backend project created

---
+++ backend/src/main/java/com/jordansilva/jscatalog/JscatalogApplication.java
@@ -0,0 +1,13 @@
+package com.jordansilva.jscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JscatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jordansilva/jscatalog/JscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jordansilva.jscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
