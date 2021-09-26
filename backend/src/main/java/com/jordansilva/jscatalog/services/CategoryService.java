03fb180646a4063b953dec675cd7ec5a0cf8f649
Jordan Silva <jordanrafael92@gmail.com>
2021-09-26 12:02:30 -0300

    Created Category DTO

--- backend/src/main/java/com/jordansilva/jscatalog/services/CategoryService.java
+++ backend/src/main/java/com/jordansilva/jscatalog/services/CategoryService.java
@@ -1,11 +1,14 @@
 package com.jordansilva.jscatalog.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.jordansilva.jscatalog.dto.CategoryDTO;
 import com.jordansilva.jscatalog.entities.Category;
 import com.jordansilva.jscatalog.repositories.CategoryRepository;
 
@@ -16,7 +19,9 @@ public class CategoryService {
 		private CategoryRepository repository;
 	
 		@Transactional(readOnly = true)
-		public List<Category> findAll() {
-			return repository.findAll();
+		public List<CategoryDTO> findAll() {
+			List<Category> list = repository.findAll();
+			
+			return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 		}
 }
