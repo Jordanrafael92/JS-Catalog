fa86be8435462a406493b720b568747fd8fa04c0
Jordan Silva <jordanrafael92@gmail.com>
2021-09-28 21:19:40 -0300

    Update (PUT) category

--- backend/src/main/java/com/jordansilva/jscatalog/services/CategoryService.java
+++ backend/src/main/java/com/jordansilva/jscatalog/services/CategoryService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,33 +13,45 @@ import org.springframework.transaction.annotation.Transactional;
 import com.jordansilva.jscatalog.dto.CategoryDTO;
 import com.jordansilva.jscatalog.entities.Category;
 import com.jordansilva.jscatalog.repositories.CategoryRepository;
-import com.jordansilva.jscatalog.services.exceptions.EntityNotFoundException;
+import com.jordansilva.jscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
 
-		@Autowired
-		private CategoryRepository repository;
-	
-		@Transactional(readOnly = true)
-		public List<CategoryDTO> findAll() {
-			List<Category> list = repository.findAll();
-			
-			return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
-		}
+	@Autowired
+	private CategoryRepository repository;
 
-		@Transactional(readOnly = true)
-		public CategoryDTO findById(Long id) {
-			Optional<Category> obj = repository.findById(id);
-			Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
-			return new CategoryDTO(entity);
-		}
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
 
-		@Transactional
-		public CategoryDTO insert(CategoryDTO dto) {
-			Category entity = new Category();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+			Category entity = repository.getOne(id);
 			entity.setName(dto.getName());
 			entity = repository.save(entity);
 			return new CategoryDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
 		}
+	}
 }
