905c5fd2c04020274d5154c2cea18e2ac988c41b
Jordan Silva <jordanrafael92@gmail.com>
2021-10-01 21:13:38 -0300

    Created product endpoint

--- backend/src/main/java/com/jordansilva/jscatalog/repositories/ProductRepository.java
+++ backend/src/main/java/com/jordansilva/jscatalog/repositories/ProductRepository.java
@@ -3,9 +3,9 @@ package com.jordansilva.jscatalog.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.jordansilva.jscatalog.entities.Category;
+import com.jordansilva.jscatalog.entities.Product;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Category, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
