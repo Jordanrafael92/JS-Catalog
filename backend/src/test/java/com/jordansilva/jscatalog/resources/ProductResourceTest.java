2968102c13a29187920501a68b32fd6be2cbc7ae
Jordan <jordanrafael92@gmail.com>
2021-10-25 19:43:32 -0300

    Refactor: call split

--- backend/src/test/java/com/jordansilva/jscatalog/resources/ProductResourceTest.java
+++ backend/src/test/java/com/jordansilva/jscatalog/resources/ProductResourceTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.springframework.http.MediaType;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -14,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import com.jordansilva.jscatalog.dto.ProductDTO;
 import com.jordansilva.jscatalog.services.ProductService;
@@ -40,6 +42,9 @@ public class ProductResourceTest {
 	
 	@Test
 	public void findAllShouldReturnPage() throws Exception{
-		mockMvc.perform(get("/products")).andExpect(status().isOk());
+		ResultActions result = mockMvc.perform(get("/products")
+				.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isOk());
 	}
 }
