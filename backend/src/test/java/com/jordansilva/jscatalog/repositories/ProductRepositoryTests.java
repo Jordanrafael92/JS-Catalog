94332e30cd7c522aa8c90ab4432ee94233836215
Jordan <jordanrafael92@gmail.com>
2021-10-30 14:11:32 -0300

    Integration test web layer - findAll

--- backend/src/test/java/com/jordansilva/jscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/jordansilva/jscatalog/repositories/ProductRepositoryTests.java
@@ -5,15 +5,10 @@ import java.util.Optional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 
-import com.jordansilva.jscatalog.dto.ProductDTO;
 import com.jordansilva.jscatalog.entities.Product;
 import com.jordansilva.jscatalog.tests.Factory;
 
