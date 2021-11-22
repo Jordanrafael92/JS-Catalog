789ce1f67ae8f429eddc0b4def911d994b46760f
Jordan <jordanrafael92@gmail.com>
2021-11-22 19:36:02 -0300

    Configured the relationship Category and Products

--- backend/src/main/java/com/jordansilva/jscatalog/entities/Category.java
+++ backend/src/main/java/com/jordansilva/jscatalog/entities/Category.java
@@ -11,6 +11,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -40,6 +41,7 @@ public class Category implements Serializable {
 		return updatedAt;
 	}
 	
+	@ManyToMany(mappedBy = "categories")
 	public Set<Product> getProducts() {
 		return products;
 	}
