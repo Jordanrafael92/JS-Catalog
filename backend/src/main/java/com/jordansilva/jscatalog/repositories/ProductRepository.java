a915554104cb845104161cbfb06ba79c881a3d79
Jordan <jordanrafael92@gmail.com>
2022-05-31 19:23:11 -0300

    Created filter for the Endpoint Products
    Test: http://localhost:8080/products?page=0&size=12&sort=name,asc&categoryId=2

--- backend/src/main/java/com/jordansilva/jscatalog/repositories/ProductRepository.java
+++ backend/src/main/java/com/jordansilva/jscatalog/repositories/ProductRepository.java
@@ -1,11 +1,19 @@
 package com.jordansilva.jscatalog.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.jordansilva.jscatalog.entities.Category;
 import com.jordansilva.jscatalog.entities.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+	
+	@Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.categories cats WHERE "
+			+ "(:category IS NULL OR :category in cats)")
+	Page<Product> find(Category category, Pageable pageable);
+	
 }
